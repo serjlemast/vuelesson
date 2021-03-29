cafba3d0237a04dfeb76e94b409e10355beb257a
SerJLemast <maksiomRpp@gmail.com>
2021-03-29 11:28:34 +0300

    Add some buttons and template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-
+<my-component />
   </div>
 </template>
 
 <script>
 
+// import MyComponent from './components/MyComponent'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'App',
   components: {
+    MyComponent
     
   }
 }
-
+</script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="root">
+        {{text}}
+        <p>
+        <button v-on:click="productValue -= 1">-</button>
+<template>{{productValue}}</template>
+        <button v-on:click="productValue += 1">+</button>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name:'MyComponent',
+    data(){
+        return{
+            
+            text:'Выбирите кол-во продуктов'
+        }
+    }
+    
+     
+}
+</script>
